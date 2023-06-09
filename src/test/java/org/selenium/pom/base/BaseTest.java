329e2cf4a35b791be4134ffb13f80cd4e98cf129
abdallamahmoud <abdallamahmoud.cs@gmail.com>
2023-06-09 15:50:08 +0200

    add Threadlocal Method

--- src/test/java/org/selenium/pom/base/BaseTest.java
+++ src/test/java/org/selenium/pom/base/BaseTest.java
@@ -6,15 +6,27 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
-    protected WebDriver driver;
+    private ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    private void setDriver(WebDriver driver){
+        this.driver.set(driver);
+    }
+    protected WebDriver getDriver(){
+        return this.driver.get();
+    }
+
+
 
     @BeforeMethod
     public void startDriver(){
-        driver = new DriverManager().initializeDriver();
+        setDriver(new DriverManager().initializeDriver());
+        System.out.println("CURRENT THREAD: " + Thread.currentThread().getId() + ", " + "DRIVER = " + getDriver());
+//        driver = new DriverManager().initializeDriver();
     }
 
     @AfterMethod
     public void quitDriver(){
-        driver.quit();
+        System.out.println("CURRENT THREAD: " + Thread.currentThread().getId() + ", " + "DRIVER = " + getDriver());
+        getDriver().quit();
     }
 }

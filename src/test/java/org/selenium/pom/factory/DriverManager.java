8253671e48f03e46e4fe9c34545cfedb5882ee29
abdallamahmoud <abdallamahmoud.cs@gmail.com>
2023-06-09 17:20:04 +0200

    change url hardcoded data to read from config.properties file

--- src/test/java/org/selenium/pom/factory/DriverManager.java
+++ src/test/java/org/selenium/pom/factory/DriverManager.java
@@ -2,6 +2,7 @@ package org.selenium.pom.factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.selenium.pom.utils.ConfigLoader;
 
 import java.time.Duration;
 
@@ -9,7 +10,8 @@ public class DriverManager {
 
     public WebDriver initializeDriver(){
         WebDriver driver = new ChromeDriver();
-        driver.get("https://askomdch.com/");
+        driver.get(ConfigLoader.getInstance().getBaseUrl());
+//        driver.get("https://askomdch.com/");
         driver.manage().window().maximize();
         return driver;
     }
